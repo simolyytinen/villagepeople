e2f997dd03d8e5df3f4f47a54b9aaab356e3368d
Simo Lyytinen <simo.lyytinen@icloud.com>
2022-03-22 20:06:55 +0200

    Projektin luonti

---
+++ server/start.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+var port = 3004;
+var hostname = "127.0.0.1";
+
+var cors = function (req, res, next)
+{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
+app.use(cors);
+
+// importataan reitit
+//const customerRoutes = require('./routes/customerRoutes');
+//app.use(customerRoutes);
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});

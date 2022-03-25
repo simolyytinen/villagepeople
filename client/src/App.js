e1763624345dc58f2806fc9bf4bb5a9d3adf4689
Simo Lyytinen <simo.lyytinen@edu.savonia.fi>
2022-03-25 19:00:41 +0200

    Lisätty materialUI fronttia

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar.js'
+import FrontPage from './components/FrontPage.js'
 
 const App = () => {
-  
   return (
     <div className="App">
         <Navbar />
+        <FrontPage />
     </div>
   );
 }

185a748f11f63491df721cca2617b8f64b94be0f
Ville Paasonen <Ville.Paasonen@edu.savonia.fi>
2022-04-06 12:48:28 +0200

    palveluhallintaa

--- client/src/components/PalveluHallinta.js
+++ client/src/components/PalveluHallinta.js
@@ -4,22 +4,43 @@ import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import Grid from "@mui/material/Grid";
 import BasicTable from "./PalveluTaulukko";
+import PalveluTaulukko from "./PalveluTaulukko";
+import PalveluForm from "./PalveluForm";
 
 const PalveluHallinta = () => {
+
+    const [palvelut, setPalvelut] = useState([]);
+    const [hae, setHae] = useState(0);
+    const [poistaId, setPoistaId] = useState(-1);
+    const [muokkaus, setMuokkaus] = useState(false);
+    const [palveluId, setPalveluId] = useState("");
+    const [nimi, setNimi] = useState("");
+    const [tyyppi, setTyyppi] = useState("");
+    const [sijainti, setSijainti] = useState("");
+    const [kuvaus, setKuvaus] = useState("");
+    const [hinta, setHinta] = useState("");
+    const [alv, setAlv] = useState("");
+    const [muokkausData, setMuokkausData] = useState("");
+    const [lisaaNimi, setLisaaNimi] = useState("");
+
+    const sarakkeet = [
+        "ID", "Sijainti", "Nimi", "Tyyppi", "Kuvaus", "Hinta", "Alv", "Muuta/Poista"
+    ];
+
+    //TÄHÄN KOODIT TIEDON HAULLE/PÄIVITYKSELLE/POISTOLLE
 
     return (
         <Container maxWidth="xl">
             <Typography variant="h4" align="center" color="text.primary" paragraph sx={{ mt: 4 }}>
                 Palveluiden hallinta
             </Typography>
+            {/* TÄTÄ VOIS KOITTAA LAITTAA VIEREKKÄIN TMS */}
             <Grid container spacing={4}>
                 <Grid item xs={12} md={8}>
-                    <Typography variant="h3" align="center" color="text.primary" paragraph sx={{ mt: 4 }}>
-                        tööt
-                    </Typography>
+                        <PalveluForm />
                 </Grid>
-                <Grid item xs={12} md={4}>
-                    <BasicTable />
+                <Grid item xs={12} md={12}>
+                    <PalveluTaulukko />
                 </Grid>
             </Grid>
 

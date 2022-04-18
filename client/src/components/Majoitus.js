c5bf67f2d04786cb3f49484b1874769ef915e721
Simo Lyytinen <simo.lyytinen@edu.savonia.fi>
2022-04-18 14:58:00 +0300

    Majoitus-välilehden checkbox

--- client/src/components/Majoitus.js
+++ client/src/components/Majoitus.js
@@ -5,6 +5,7 @@ import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import { FormControlLabel, Checkbox } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Kortti from './Kortti';
 import DatePickers from './DatePicker';
@@ -15,20 +16,32 @@ const theme = createTheme();
 export default function Majoitus() {
 
   const { server } = useContext(DataContext);
-  const [hae, setHae] = useState(0);
   const [mokit, setMokit] = useState([]);
+  const [toimipaikat, setToimipaikat] = useState([]);
 
   const [hakuehto, setHakuehto] = useState("");
   const [alkuPvm, setAlkuPvm] = useState("");
   const [loppuPvm, setLoppuPvm] = useState("");
 
-  // const tyhjenna = () =>{
-  //   setHakuehto("");
-  // }
+  const [isChecked, setIsChecked] = useState([]);
+
+  // Toimipisteiden hakeminen tietokannasta checkboxeja varten
+  useEffect(()=>{
+    fetch(server + "/api/toimipisteet")
+    .then(response => response.json())
+    .then((data) => {
+        console.log(data)
+        setToimipaikat(data)
+        setIsChecked(() => toimipaikat.map((i) => false))
+      }
+      ) 
+    .catch(err => console.log(err));
+}, [server])
 
   useEffect(()=>{
+    console.log(hakuehto);
     const funktio = () => {
-        let api = server + "/api/mokit/"/*  + alueId */;
+        let api = server + "/api/mokit/";
         fetch(api)
         .then(response => response.json())
         .then((data) => {
@@ -36,8 +49,41 @@ export default function Majoitus() {
             setMokit(data)})
         .catch(err => console.log(err));
     }
-    /* if (alueId !== "")  */funktio();
-}, [hae, /* alueId, */ server])
+    if (hakuehto !== "")  funktio();
+}, [hakuehto, server])
+
+const haeClicked = () => {
+  let alueet = [];
+  for (let i=0; i<isChecked.length; i++) {
+    if (isChecked[i]) alueet.push(i+1);
+  }
+  
+  setHakuehto({
+    alueet: alueet,
+    alkupvm: alkuPvm,
+    loppupvm: loppuPvm
+  });  
+}
+
+
+const Tyhjenna = () =>{
+  setHakuehto("");
+  setIsChecked(() => {
+    return isChecked.map((c) => {
+      return false;
+    });
+  });
+}
+
+// checkboxien apufunktiot
+const isCheckboxChecked = (index, value) => {
+  setIsChecked(() => {
+    return isChecked.map((c, i) => {
+      if (i === index) return value;
+      return c;
+    });
+  });
+};
 
   return (
     <ThemeProvider theme={theme}>
@@ -61,6 +107,19 @@ export default function Majoitus() {
             >
               Majoitus
             </Typography>
+            {/* Alueen valinnan checkbox */}
+            <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: "center"}}>
+              {toimipaikat.map((a, index)=>{
+                return (
+                  <FormControlLabel
+                  key={a.alue_id}
+                  label={a.nimi}
+                  control={<Checkbox checked={isChecked[index] ? isChecked[index] : false} onChange={(e) => isCheckboxChecked(index, e.target.checked)} />}
+                  /> 
+                )
+            })}
+    
+            </Box>
             {/* <Typography variant="h5" align="center" color="text.secondary" paragraph>
             </Typography> */}
             <Stack
@@ -78,8 +137,8 @@ export default function Majoitus() {
               spacing={2}
               justifyContent="center"
             >
-              <Button variant="contained">Hae majoituskohteet</Button>
-              <Button variant="outlined">Tyhjennä</Button>
+              <Button variant="contained" onClick={()=>{haeClicked()}}>Hae majoituskohteet</Button>
+              <Button variant="outlined" onClick={()=>{Tyhjenna()}}>Tyhjennä</Button>
             </Stack>
           </Container>
         </Box>
